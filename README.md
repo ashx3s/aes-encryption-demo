505388fb59552cf4408257393e4912fcf23af77e
ashx3s <ashlyn@lilyx.io>
2025-04-02 18:07:29 -0600

    finished decryption and encryption examples

---
+++ README.md
@@ -0,0 +1,8 @@
+# AES Encryption Demo
+
+Experiment with using the crypto module to encrypt and decrypt information.
+
+## Resources and Quick Notes
+
+- [Crypto Docs createCipheriv](https://nodejs.org/docs/latest-v22.x/api/crypto.html#cryptocreatecipherivalgorithm-key-iv-options)
+  - creates and returns a cipher object with algorithm, key, and initialization vector
--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+"use strict";
 const crypto = require("crypto");
 
 // aes-128-cbc encryption
-function encrypt(text, secretKey) {}
+function encrypt(text, secretKey) {
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv("aes-128-cbc", secretKey, iv);
+  let encrypted = cipher.update(text, "utf8", "hex");
+  encrypted += cipher.final("hex");
+  return { encryptedMessage: encrypted, iv: iv.toString("hex") };
+}
 
-function decrypt(encryptedMessage, secretKey, ivHex) {}
+function decrypt(encryptedMessage, secretKey, ivHex) {
+  const iv = Buffer.from(ivHex, "hex");
+  const decipher = crypto.createDecipheriv("aes-128-cbc", secretKey, iv);
+  let decrypted = decipher.update(encryptedMessage, "hex", "utf8");
+  decrypted += decipher.final("utf8");
+  return decrypted;
+}
 
 const secretKey = Buffer.from("My-Secret-Key123");
 const textToEncrypt = "This is a secret message";
@@ -12,3 +25,6 @@ const { encryptedMessage, iv } = encrypt(textToEncrypt, secretKey);
 
 console.log("Encrypted Message: ", encryptedMessage);
 console.log("IV: ", iv);
+
+const decryptedMessage = decrypt(encryptedMessage, secretKey, iv);
+console.log("Decrypted Message: ", decryptedMessage);
